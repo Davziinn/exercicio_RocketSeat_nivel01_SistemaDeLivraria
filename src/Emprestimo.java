2616c64075153e30d49f7a1a51a6eff24f96d993
Davi <mdavi6085@gmail.com>
2025-01-27 15:46:22 -0300

    classes moldes criadas

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -1,2 +1,87 @@
+import java.util.Date;
+
 public class Emprestimo {
+    private int id;
+    private static int contadorId = 1;
+    private Livro livro;
+    private String nomeUsuario;
+    private Date dataEmprestimo;
+    private Date dataDevolucao;
+    private boolean ativo;
+
+    public Emprestimo(int id, Livro livro, String nomeUsuario, boolean ativo, Date dataEmprestimo) {
+        this.id = contadorId++;
+        this.livro = livro;
+        this.nomeUsuario = nomeUsuario;
+        this.dataEmprestimo = new Date();
+        this.ativo = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getContadorId() {
+        return contadorId;
+    }
+
+    public static void setContadorId(int contadorId) {
+        Emprestimo.contadorId = contadorId;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo{" +
+                "id=" + id +
+                ", livro=" + livro +
+                ", nomeUsuario='" + nomeUsuario + '\'' +
+                ", dataEmprestimo=" + dataEmprestimo +
+                ", dataDevolucao=" + dataDevolucao +
+                ", ativo=" + ativo +
+                '}';
+    }
 }
--- src/Livro.java
+++ src/Livro.java
@@ -1,2 +1,79 @@
+import java.util.Date;
+
 public class Livro {
+    private int id;
+    private String titulo;
+    private Autor autor;
+    private boolean disponivel;
+    private Date dataCadastro;
+    private Date dataAtualizado;
+
+    public Livro(int id, String titulo, Autor autor, boolean disponivel, Date dataAtualizado, Date dataCadastro) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+        this.dataAtualizado = new Date();
+        this.dataCadastro = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Date getDataAtualizado() {
+        return dataAtualizado;
+    }
+
+    public void setDataAtualizado(Date dataAtualizado) {
+        this.dataAtualizado = dataAtualizado;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor=" + autor +
+                ", disponivel=" + disponivel +
+                ", dataCadastro=" + dataCadastro +
+                ", dataAtualizado=" + dataAtualizado +
+                '}';
+    }
 }
